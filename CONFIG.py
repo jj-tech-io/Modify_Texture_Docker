7f2be6b54a00d638f1a99661238cb43dd45e760d
Joel Johnson <60250054+jj-tech-io@users.noreply.github.com>
2024-01-06 14:40:16 -0800

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -4,6 +4,7 @@
 # C:\Users\joeli\Dropbox\Code\Python Projects\Modify_Texture_Docker\saved_models\2024_01_03\decoder.h5
 # ENCODER_PATH =  r"C:\Users\joeli\Dropbox\Code\Python Projects\Modify_Texture_Docker\saved_models\2024_01_03\encoder.h5"
 # DECODER_PATH =  r"C:\Users\joeli\Dropbox\Code\Python Projects\Modify_Texture_Docker\saved_models\2024_01_03\decoder.h5"
+import pathlib
 RUN_LOCAL = False
 if RUN_LOCAL:
   ENCODER_PATH = r"saved_models/encoder.h5"
@@ -17,3 +18,7 @@ else:
   DECODER_PATH = "/content/Modify_Texture_Docker/saved_models/316/decoder.h5"
   ENCODER_PATH = "/content/Modify_Texture_Docker/saved_models/no_duplicates_75_2_mask/encoder.h5"
   DECODER_PATH = "/content/Modify_Texture_Docker/saved_models/no_duplicates_75_2_mask/decoder.h5"
+  ENCODER_PATH = "saved_models\316\decoder.h5"
+  DECODER_PATH = "saved_models\316\decoder.h5"
+  ENCODER_PATH = pathlib.Path(ENCODER_PATH)
+  DECODER_PATH = pathlib.Path(DECODER_PATH)
