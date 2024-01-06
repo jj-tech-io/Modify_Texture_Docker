c9b3e2608f3d1ca66b088c006a590f992e2d00ac
Joel Johnson <60250054+jj-tech-io@users.noreply.github.com>
2024-01-06 14:32:44 -0800

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -4,9 +4,16 @@
 # C:\Users\joeli\Dropbox\Code\Python Projects\Modify_Texture_Docker\saved_models\2024_01_03\decoder.h5
 # ENCODER_PATH =  r"C:\Users\joeli\Dropbox\Code\Python Projects\Modify_Texture_Docker\saved_models\2024_01_03\encoder.h5"
 # DECODER_PATH =  r"C:\Users\joeli\Dropbox\Code\Python Projects\Modify_Texture_Docker\saved_models\2024_01_03\decoder.h5"
-ENCODER_PATH = r"saved_models/encoder.h5"
-DECODER_PATH = r"saved_models/decoder.h5"
-DECODER_PATH = r"C:\Users\joeli\Dropbox\Code\HM_2023\TrainedModels\no_duplicates_75_2_mask\decoder.h5"
-ENCODER_PATH = r"C:\Users\joeli\Dropbox\Code\HM_2023\TrainedModels\no_duplicates_75_2_mask\encoder.h5"
-ENCODER_PATH = r"C:\Users\joeli\Dropbox\AE_MC\saved_ml_models\SmallBatchSize\encoder_19_05.h5"
-DECODER_PATH = r"C:\Users\joeli\Dropbox\AE_MC\saved_ml_models\SmallBatchSize\decoder_19_05.h5"
+RUN_LOCAL = False
+if RUN_LOCAL:
+  ENCODER_PATH = r"saved_models/encoder.h5"
+  DECODER_PATH = r"saved_models/decoder.h5"
+  DECODER_PATH = r"C:\Users\joeli\Dropbox\Code\HM_2023\TrainedModels\no_duplicates_75_2_mask\decoder.h5"
+  ENCODER_PATH = r"C:\Users\joeli\Dropbox\Code\HM_2023\TrainedModels\no_duplicates_75_2_mask\encoder.h5"
+  ENCODER_PATH = r"C:\Users\joeli\Dropbox\AE_MC\saved_ml_models\SmallBatchSize\encoder_19_05.h5"
+  DECODER_PATH = r"C:\Users\joeli\Dropbox\AE_MC\saved_ml_models\SmallBatchSize\decoder_19_05.h5"
+else:
+  ENCODER_PATH = "/content/Modify_Texture_Docker/saved_models/316/encoder.h5"
+  DECODER_PATH = "/content/Modify_Texture_Docker/saved_models/316/decoder.h5"
+  ENCODER_PATH = "/content/Modify_Texture_Docker/saved_models/no_duplicates_75_2_mask/encoder.h5"
+  DECODER_PATH = "/content/Modify_Texture_Docker/saved_models/no_duplicates_75_2_mask/decoder.h5"
